e0843de828a9da2f5a6971ca990326e6d457c465
Hamid Rajabi <rajabihamid75@gmail.com>
2022-08-01 22:45:38 +0430

    A lot of changes added and edit feature for the education part approved. but the code is not clean yet. so in the future I clean it

--- src/components/EducationalInformation.js
+++ src/components/EducationalInformation.js
@@ -9,13 +9,25 @@ class EducationalInformation extends React.Component {
 
         this.state = {
             addingStatus: false,
+            editingStatus: false,
             values: {
                 id: uniqid(),
+                title: "",
+                university: "",
+                fromYear: "",
+                toYear: "",
+                degree: "",
+                grade: ""
+            },
+            editValues: {
+                id: "",
+                title: "",
                 university: "",
                 fromYear: "",
                 toYear: "",
                 degree: "",
                 grade: "",
+                row: ""
             },
             edus: [],
         }
@@ -25,13 +37,18 @@ class EducationalInformation extends React.Component {
         this.handleCancel = this.handleCancel.bind(this);
         this.handleAdding = this.handleAdding.bind(this);
         this.handleDelete = this.handleDelete.bind(this);
+        this.handleEditEdu = this.handleEditEdu.bind(this);
+        this.handleEditChange = this.handleEditChange.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
     }
 
     handleAddingStatus() {
         this.setState(prevState => ({
+            editingStatus: false,
             addingStatus: true,
             values: {                  
-                ...prevState.values,    
+                ...prevState.values,
+                title: "",    
                 university: "",
                 fromYear: "",
                 toYear: "",
@@ -43,7 +60,8 @@ class EducationalInformation extends React.Component {
 
     handleCancel() {
         this.setState({
-            addingStatus: false
+            addingStatus: false,
+            editingStatus: false
         })
     }
 
@@ -61,6 +79,7 @@ class EducationalInformation extends React.Component {
             edus: this.state.edus.concat(this.state.values),
             values: {
                 id: uniqid(),
+                title: "",
                 university: "",
                 fromYear: "",
                 toYear: "",
@@ -81,41 +100,123 @@ class EducationalInformation extends React.Component {
             })
         }
     }
+
+    handleEditEdu(e) {
+        const intendedEdu = this.state.edus.filter(d => d.id === e.target.name)[0]
+        this.setState({
+            addingStatus: false,
+            editingStatus: true,
+            editValues: {
+                id: intendedEdu.id,
+                title: intendedEdu.title,
+                university: intendedEdu.university,
+                fromYear: intendedEdu.fromYear,
+                toYear: intendedEdu.toYear,
+                degree: intendedEdu.degree,
+                grade: intendedEdu.grade,
+                row: e.target.dataset.row
+            },
+
+        })
+    }
+
+    handleEditChange(e) {
+        this.setState(prevState => ({
+            editValues : {
+                ...prevState.editValues,
+                [e.target.name]: e.target.value
+            }
+        }))
+    }
+
+    handleUpdate() {
+        this.state.edus.map(e => {
+            if(e.id === this.state.editValues.id) {
+                e.title = this.state.editValues.title;
+                e.university = this.state.editValues.university;
+                e.fromYear = this.state.editValues.fromYear;
+                e.toYear = this.state.editValues.toYear;
+                e.degree = this.state.editValues.degree;
+                e.grade = this.state.editValues.grade;
+            }
+        })
+        this.setState({
+            editingStatus: false
+        })
+        // console.log("Update");
+    }
     
     render() {
-        const {addingStatus, values, edus} = this.state;
+        const {addingStatus, values, editValues, edus, editingStatus} = this.state;
+        console.log(edus);
         return (
             <div className="edu-container">
                 <h2>Education: </h2>
-                <Educations data={edus} handleDeleteEdu={this.handleDelete}/>
+
+                {editingStatus ? 
+                    <div className="edu-forms-container">
+                        <p>You're editing <b>{edus.filter(e => e.id === editValues.id)[0].title}</b></p>
+                        
+                        <div className="add-edu update-edu">
+                            <div className="left-inputs">
+                                <label htmlFor="title"><b>Title:</b> </label>
+                                <input id="title" placeholder="Enter the title" value={editValues.title} name="title" onChange={this.handleEditChange}/>
+                                <label htmlFor="university"><b>University's Name:</b> </label>
+                                <input id="university" placeholder="Enter the name ..." value={editValues.university} name="university" onChange={this.handleEditChange}/>
+                                <label htmlFor="degree"><b>Degree:</b> </label>
+                                <input id="degree" placeholder="Your Degree" value={editValues.degree} name="degree" onChange={this.handleEditChange}/>
+
+                            </div>
+                        
+                            <div className="right-inputs">
+                                <label htmlFor="from-year"><b>From:</b> </label>
+                                <input id="from-year" placeholder="YYYY" value={editValues.fromYear} name="fromYear" onChange={this.handleEditChange}/>
+                                <label htmlFor="to-year"><b>To:</b> </label>
+                                <input id="to-year" placeholder="YYYY" value={editValues.toYear} name="toYear" onChange={this.handleEditChange}/>
+                                <label htmlFor="grade"><b>Grade:</b> </label>
+                                <input id="grade" placeholder="Your Final Grade" value={editValues.grade} name="grade" onChange={this.handleEditChange}/>
+                            </div>
+                        </div>
+                        <div className="adding-btns">
+                            <button className="btn add-btn" onClick={this.handleUpdate}>Update</button>
+                            <button className="btn cancel-btn" onClick={this.handleCancel}>Cancel</button>
+                        </div>
+                    </div>
+                :
+                    null
+                }
+
+                <Educations data={edus} handleDeleteEdu={this.handleDelete} handleEditEdu={this.handleEditEdu}/>
 
                 {!addingStatus ? 
                     <button className="btn add-btn" onClick={this.handleAddingStatus}>+Add</button>
                 
                 :
+                
+                <div className="edu-forms-container">
                     <div className="add-edu">
                         <div className="left-inputs">
-                            <label htmlFor="university">University's Name: </label>
+                            <label htmlFor="title"><b>Title:</b> </label>
+                            <input id="title" placeholder="Enter the title" value={values.title} name="title" onChange={this.handleChange}/>
+                            <label htmlFor="university"><b>University's Name:</b> </label>
                             <input id="university" placeholder="Enter the name ..." value={values.university} name="university" onChange={this.handleChange}/>
-                            <div className="year-inputs">
-                                <label htmlFor="from-year">From: </label>
-                                <input id="from-year" placeholder="YYYY" value={values.fromYear} name="fromYear" onChange={this.handleChange}/>
-                                <label htmlFor="to-year">To: </label>
-                                <input id="to-year" placeholder="YYYY" value={values.toYear} name="toYear" onChange={this.handleChange}/>
-                            </div>
+                            <label htmlFor="degree"><b>University's Name:</b> </label>
+                            <input id="degree" placeholder="Your Degree" value={values.degree} name="degree" onChange={this.handleChange}/>
                         </div>
                         <div className="right-inputs">
-                            <label htmlFor="degree">Degree: </label>
-                            <input id="degree" placeholder="Your Degree" value={values.degree} name="degree" onChange={this.handleChange}/>
-                            <label htmlFor="grade">Grade: </label>
+                            <label htmlFor="from-year"><b>From:</b> </label>
+                            <input id="from-year" placeholder="YYYY" value={values.fromYear} name="fromYear" onChange={this.handleChange}/>
+                            <label htmlFor="to-year"><b>To:</b> </label>
+                            <input id="to-year" placeholder="YYYY" value={values.toYear} name="toYear" onChange={this.handleChange}/>
+                            <label htmlFor="grade"><b>Grade:</b> </label>
                             <input id="grade" placeholder="Your Final Grade" value={values.grade} name="grade" onChange={this.handleChange}/>
-
-                            <div className="adding-btns">
-                                <button className="btn add-btn" onClick={this.handleAdding}>Add</button>
-                                <button className="btn cancel-btn" onClick={this.handleCancel}>Cancel</button>
-                            </div>
                         </div>
                     </div>
+                    <div className="adding-btns">
+                        <button className="btn add-btn" onClick={this.handleAdding}>Add</button>
+                        <button className="btn cancel-btn" onClick={this.handleCancel}>Cancel</button>
+                    </div>
+                </div>
                 }
 
             </div>  
--- src/components/Educations.js
+++ src/components/Educations.js
@@ -7,24 +7,30 @@ class Educations extends React.Component {
     }
 
     render() {
-        const edus = this.props.data;
+        const {data, handleDeleteEdu, handleEditEdu} = this.props;
         return (
             <div className="edulist-container">
 
-                {edus.map(e => (
+                {data.map((e, i) => (
                     <div key={e.id} className="eduitems">
-                        <div>
+                        <div className="row">
+                            <h3>{i + 1}</h3>
+                        </div>
+                        <div className="long-contents">
+                            <p><b>Title:</b> {e.title}</p>
                             <p><b>University:</b> {e.university}</p>
-                            <p><b>From Year:</b> {e.fromYear}</p>
-                        </div>
-                        <div>
                             <p><b>Degree:</b> {e.degree}</p>
-                            <p><b>To Year:</b> {e.toYear}</p>
                         </div>
                         <div>
+                            <p><b>From Year:</b> {e.fromYear}</p>
+                            <p><b>To Year:</b> {e.toYear}</p>
                             <p><b>Grade:</b> {e.grade}</p>
-                            <button className="edit-btn">Edit</button>
-                            <button name={e.id} onClick={this.props.handleDeleteEdu} className="delete-btn">Delete</button>
+                        </div>
+                        {/* <div className="grade">
+                        </div> */}
+                        <div className="btns-container">
+                            <button data-row={e.title} name={e.id} onClick={handleEditEdu} className="edit-btn">Edit</button>
+                            <button name={e.id} onClick={handleDeleteEdu} className="delete-btn">Delete</button>
                         </div>
                     </div>
                 ))}
